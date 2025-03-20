805ab6b5e91d06e78339905a62abe03ee4a6f87e
Satish Karra <skarra4@gitam.in>
2025-03-20 15:36:05 +0530

    fix tags for upload

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -33,20 +33,8 @@ const Student = ({ details,back, link, mode ,ondelete }) => {
   },[details,showUpload,toggle]);
 
 useEffect(()=>{
-    async function student(){
-      const res = await fetch(`/api/deets/${details.username}`);
-      if(res.status === 200){
-          const data = await res.json();
-          details.tags = data.tags;
-      }else{
-          alert('User not found');
-      }   
-    }
-    if(details.username!==''){
-      student();
-    }
-
-  },[toggle,showUpload]);
+   ondelete();
+  },[showUpload]);
   function handleDelete() {
     setToggle((prev)=>{return !prev});
     ondelete();
