5a14710031916fbac4dc4ea288223e75d4ff137f
Chaitanya Vedula <118042946+Chaitanya1130@users.noreply.github.com>
2025-03-24 10:59:21 +0530

    return role in all files

--- client/src/backend/route.js
+++ client/src/backend/route.js
@@ -121,7 +121,7 @@ router.get('/allfiles/:username',async (req,res)=>{
             return res.status(404).json({message: "User not found"});
         }
         
-        return res.status(200).json({files: user.files});
+        return res.status(200).json({files: user.files,role:user.role});
     } catch (error) {
         console.error("Error fetching files:", error);
         return res.status(500).json({message: "Server error"});
